1fb1156bd44b0d60fcdf9b11cb71de0686dcdcd7
Stefan Stefanov <stefan.bg@gmail.com>
2013-03-12 11:15:30 +0200

    Fixed failing test case

--- Test/Case/View/Helper/BootstrapHtmlHelperTest.php
+++ Test/Case/View/Helper/BootstrapHtmlHelperTest.php
@@ -30,18 +30,18 @@ class BootstrapHtmlHelperTest extends CakeTestCase {
 		// single
 		$icon = 'glass';
 		$result = $this->BootstrapHtml->link($title, $url, compact('icon'));
-		$expected = '<a href="/"><i class="icon-glass"></i> &lt;b&gt;test&lt;/b&gt;</a>';
+		$expected = '<a href="/"><i class="icon-glass"></i>&lt;b&gt;test&lt;/b&gt;</a>';
 		$this->assertSame($expected, $result, 'normal icon link with escape string');
 		// white icon
 		$icon = 'glass white';
 		$result = $this->BootstrapHtml->link($title, $url, compact('icon'));
-		$expected = '<a href="/"><i class="icon-glass icon-white"></i> &lt;b&gt;test&lt;/b&gt;</a>';
+		$expected = '<a href="/"><i class="icon-glass icon-white"></i>&lt;b&gt;test&lt;/b&gt;</a>';
 		$this->assertSame($expected, $result, 'white icon link with escape string');
 		// white icon without escape
 		$icon = 'glass white';
 		$escape = false;
 		$result = $this->BootstrapHtml->link($title, $url, compact('icon', 'escape'));
-		$expected = '<a href="/"><i class="icon-glass icon-white"></i> <b>test</b></a>';
+		$expected = '<a href="/"><i class="icon-glass icon-white"></i><b>test</b></a>';
 		$this->assertSame($expected, $result, 'white icon link without escape');
 	}
 
